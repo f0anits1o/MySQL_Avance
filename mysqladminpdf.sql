04d05fd21b6a623554b401d207bf106aaec78637
f0anits1o <avotriniainafidelis@gmail.com>
2024-01-18 13:12:37 +0300

    Curseurs et utilisation avancees des bloc d'instructions

--- mysqladminpdf.sql
+++ mysqladminpdf.sql
@@ -3365,6 +3365,191 @@ SELECT id, nom, espece_id, prix FROM Race;
         -- V.6.2.2. Restrictions
         -- V.6.2.3. Parcourir intelligement tous les resultats d'un curseur
 
+#########################################################################################
+#                                                                                       #
+#               Quatre étapes sont nécessaires pour utiliser un curseur.                #
+#                                                                                       #
+#    — Déclaration du curseur : avec une instruction DECLARE.                           #
+#    — Ouverture du curseur : on exécute la requête SELECT du curseur et on stocke      #
+#        le résultat dans celui-ci.                                                     #
+#                                                                                       #
+#    — Parcours du curseur : on parcourt une à une les lignes.                          #
+#    — Fermeture du curseur.                                                            #
+#                                                                                       #
+#########################################################################################
+
+
+
+
+        -- V.6.2.1. Syntaxe
+        -- ----------------
+            -- V.6.2.1.1. Déclaration du curseur
+            -- V.6.2.1.2. Ouverture et fermeture du curseur
+            -- V.6.2.1.3. Parcours du curseur
+
+            -- V.6.2.1.1. Déclaration du curseur:
+            -- ---------------------------------
+                DECLARE nom_curseur CURSOR FOR requete_select;
+
+                -- Exemple :
+                DECLARE curseur_client CURSOR
+                FOR SELECT *
+                FROM Client;
+
+
+            -- V.6.2.1.2. Ouverture et fermeture du curseur
+            -- --------------------------------------------
+                OPEN nom_curseur;
+                -- Parcours du curseur et instructions diverses
+                CLOSE nom_curseur;
+
+
+            -- V.6.2.1.3. Parcours du curseur
+            -- ------------------------------
+                FETCH nom_curseur INTO variable(s);
+
+                -- Exemple : la procédure suivante parcourt les deux premières lignes de la table Client
+                -- avec un curseur.
+                
+                DELIMITER |
+                CREATE PROCEDURE parcours_deux_clients()
+                BEGIN
+                    DECLARE v_nom, v_prenom VARCHAR(100);
+
+                    DECLARE curs_clients CURSOR
+                    FOR SELECT nom, prenom -- Le SELECT récupère deux colonnes
+                    FROM Client
+                    ORDER BY nom, prenom; -- On trie les clients par ordre alphabétique
+
+                    OPEN curs_clients; -- Ouverture du curseur
+
+                        FETCH curs_clients INTO v_nom, v_prenom; -- On récupère la première ligne et on assigne les valeur               récupérées à nos variables locales
+                        SELECT CONCAT(v_prenom, ' ', v_nom) AS 'Premier client';
+
+                        FETCH curs_clients INTO v_nom, v_prenom; -- On récupère la seconde ligne et on assigne les valeur               -- récupérées à nos variables locales
+                        SELECT CONCAT(v_prenom, ' ', v_nom) AS 'Second client';
+
+                    CLOSE curs_clients; -- Fermeture du curseur
+                END;
+                DELIMITER ;
+
+                CALL parcours_deux_clients();
+
+
+
+        -- V.6.2.2. Restrictions:
+        -- ---------------------
+            # jereo ny lesona fa faibless MySQL io.
+
+
+
+        -- V.6.2.3. Parcourir intelligement tous les resultats d'un curseur
+        -- ----------------------------------------------------------------
+
+            -- V.6.2.3.1. Condition d’arrêt
+            -- V.6.2.3.2. Le cas des booléens chez MySQL
+            
+
+            -- V.6.2.3.1. Condition d’arrêt
+            -- ----------------------------
+                 DELIMITER |
+                CREATE PROCEDURE test_condition(IN p_ville VARCHAR(100))
+                BEGIN
+                    DECLARE v_nom, v_prenom VARCHAR(100);
+
+                    DECLARE curs_clients CURSOR
+                        FOR SELECT nom, prenom
+                        FROM Client
+                        WHERE ville = p_ville;
+
+                    OPEN curs_clients;
+
+                        LOOP
+                            FETCH curs_clients INTO v_nom, v_prenom;
+                            SELECT CONCAT(v_prenom, ' ', v_nom) AS 'Client';
+                        END LOOP;
+
+                    CLOSE curs_clients;
+                END;
+                DELIMITER ;
+
+                CALL test_condition('Houtsiplou');
+
+                # Exemple : la procédure test_condition2() ci-dessous fait la même chose que test_condition(),
+                # mais inclut le gestionnaire d’erreur et le IF nécessaires pour stopper la boucle dès que toutes les
+                # lignes sélectionnées ont été parcourues :             DELIMITER |
+                
+                Delimiter |
+                CREATE PROCEDURE test_condition2(IN p_ville VARCHAR(100))
+                BEGIN
+                    DECLARE v_nom, v_prenom VARCHAR(100);
+                    DECLARE fin TINYINT DEFAULT 0; -- Variable locale utilisée pour stopper la boucle
+
+                    DECLARE curs_clients CURSOR
+                        FOR SELECT nom, prenom
+                        FROM Client
+                        WHERE ville = p_ville;
+
+                    DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin = 1; -- Gestionnaire d'erreur pour la condition NOT FOUND
+
+                    OPEN curs_clients;
+
+                        loop_curseur: LOOP
+                            FETCH curs_clients INTO v_nom, v_prenom;
+
+                            IF fin = 1 THEN -- Structure IF pour quitter la boucle à la fin des résultats
+                                LEAVE loop_curseur;
+                            END IF;
+
+                            SELECT CONCAT(v_prenom, ' ', v_nom) AS 'Client';
+                        END LOOP;
+
+                    CLOSE curs_clients;
+                END;
+                DELIMITER ;
+
+                CALL test_condition2('Houtsiplou');
+                CALL test_condition2('Bruxelles');
+
+
+
+            -- V.6.2.3.2. Le cas des booléens chez MySQL
+            -- -----------------------------------------
+                SELECT 1 = 1, 1 = 2; -- 1 = 1 est vrai, bien sûr. Contrairement à 1 = 2 (si si !                DROP PROCEDURE test_condition2;
+                
+
+                DROP PROCEDURE test_condition2;
+
+                DELIMITER |
+                CREATE PROCEDURE test_condition2(IN p_ville VARCHAR(100))
+                BEGIN
+                    DECLARE v_nom, v_prenom VARCHAR(100);
+                    DECLARE fin BOOLEAN DEFAULT FALSE; -- On déclare fin comme un BOOLEAN, avec FALSE pour défaut
+
+                    DECLARE curs_clients CURSOR
+                        FOR SELECT nom, prenom
+                        FROM Client
+                        WHERE ville = p_ville;
+
+                    DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin = TRUE; -- On utilise TRUE au lieu de 1
+
+                    OPEN curs_clients;
+
+                        loop_curseur: LOOP
+                            FETCH curs_clients INTO v_nom, v_prenom;
+
+                            IF fin THEN -- Plus besoin de "= 1"
+                            LEAVE loop_curseur;
+                            END IF;
+
+                            SELECT CONCAT(v_prenom, ' ', v_nom) AS 'Client';
+                        END LOOP;
+
+                    CLOSE curs_clients;
+                END;
+                DELIMITER ;
+
+
 
     -- V.6.3. Utilisation avancees des blocs d'instructions
     -- ----------------------------------------------------
@@ -3373,6 +3558,100 @@ SELECT id, nom, espece_id, prix FROM Race;
         -- V.6.3.3. Transactions et gestion d'erreurs
         -- V.6.3.4. Preparer une requete dans un bloc d'instructions
         -- V.6.3.5. En resumee
+
+
+        -- V.6.3.1. Utiliser des variables utilisateur dans un bloc d'instructions
+        -- -----------------------------------------------------------------------
+        DELIMITER |
+        CREATE PROCEDURE test_vu()
+        BEGIN
+            SET @var = 15;
+        END;
+        DELIMITER ;
+
+        SELECT @var; -- @var n'existe pas encore, on ne l'a pas définie
+        CALL test_vu(); -- On exécute la procédure
+        SELECT @var; -- @var vaut maintenant 15, même en dehors de la procédure, puisqu'elle est définie partout dans la session
+
+        SET @var = 'Bonjour';
+        CALL test_vu();
+        SELECT @var; -- Donne 15 !  
+
+
+        -- V.6.3.2. Utiliser un procedure dans un bloc
+        -- -------------------------------------------
+
+            DELIMITER |
+            CREATE PROCEDURE carre(INOUT p_nb FLOAT) SET p_nb = p_nb * p_nb;
+                    CREATE PROCEDURE surface_cercle(IN p_rayon FLOAT, OUT p_surface FLOAT)
+            BEGIN
+                CALL carre(p_rayon);
+                        SET p_surface = p_rayon * PI();
+            END;
+            DELIMITER ;
+                    CALL surface_cercle(1, @surface); -- Donne environ pi (3,14...)
+            SELECT @surface;
+            CALL surface_cercle(2, @surface); -- Donne environ 12,57...
+            SELECT @surface;
+
+
+        -- V.6.3.3. Transactions et gestion d'erreurs
+        -- ----------------------------------------                  
+            DELIMITER |
+            
+            CREATE PROCEDURE adoption_deux_ou_rien(p_client_id INT, p_animal_id_1 INT, p_animal_id_2 INT)
+            BEGIN
+                DECLARE v_prix DECIMAL(7,2);
+
+                DECLARE EXIT HANDLER FOR SQLEXCEPTION ROLLBACK; -- Gestionnaire qui annule la transaction et termine la procédure
+
+                START TRANSACTION;
+
+                    SELECT COALESCE(Race.prix, Espece.prix) INTO v_prix
+                    FROM Animal
+                    INNER JOIN Espece ON Espece.id = Animal.espece_id
+                    LEFT JOIN Race ON Race.id = Animal.race_id
+                    WHERE Animal.id = p_animal_id_1;
+
+                    INSERT INTO Adoption (animal_id, client_id, date_reservation, date_adoption, prix, paye)
+                    VALUES (p_animal_id_1, p_client_id, CURRENT_DATE(), CURRENT_DATE(), v_prix, TRUE);
+
+                    SELECT 'Adoption animal 1 réussie' AS message;
+
+                    SELECT COALESCE(Race.prix, Espece.prix) INTO v_prix
+                    FROM Animal
+                    INNER JOIN Espece ON Espece.id = Animal.espece_id
+                    LEFT JOIN Race ON Race.id = Animal.race_id
+                    WHERE Animal.id = p_animal_id_2;
+
+                    INSERT INTO Adoption (animal_id, client_id, date_reservation, date_adoption, prix, paye)
+                    VALUES (p_animal_id_2, p_client_id, CURRENT_DATE(), CURRENT_DATE(), v_prix, TRUE);
+
+                    SELECT 'Adoption animal 2 réussie' AS message;
+
+                COMMIT;
+            END;
+            DELIMITER ;
+
+            CALL adoption_deux_ou_rien(2, 43, 55); -- L'animal 55 a déjà été adopté
+
+
+        -- V.6.3.4. Preparer une requete dans un bloc d'instructions
+        -- ---------------------------------------------------------
+
+            -- Exemple : la procédure suivante ajoute la ou les clauses que l’on veut à une simple requête
+            DELIMITER |
+            CREATE PROCEDURE select_race_dynamique(p_clause VARCHAR(255))
+            BEGIN
+                SET @sql = CONCAT('SELECT nom, description FROM Race ', p_clause);
+
+                PREPARE requete FROM @sql;
+                EXECUTE requete;
+            END;
+            DELIMITER ;
+
+            CALL select_race_dynamique('WHERE espece_id = 2'); -- Affichera les races de chats
+            CALL select_race_dynamique('ORDER BY nom LIMIT 2'); -- Affichera les deux premières races par ordre alphabétique de leur nom
 
 
 
@@ -3490,7 +3769,4 @@ SELECT id, nom, espece_id, prix FROM Race;
                     WHERE id = 20;
     -- V.7.5. Restrictions
         -- V.7.5.1. En resume
-
-
-
-v.    
+   
